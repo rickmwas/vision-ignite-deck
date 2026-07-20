d7613a9f3ec04ba863a5b7b07d24c4e3e8dfb934
rickmwas <rickmwasswiz@gmail.com>
2026-07-20 23:18:32 +0300

    ass vercel o solve 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -19,6 +20,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     tailwindcss(),
     tsConfigPaths(),
     react(),
